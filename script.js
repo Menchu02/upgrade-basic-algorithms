017edb2fcda216e40c60a809afbc1e5b26dcb858
Carmen Blanco <menxuuu02@gmail.com>
2021-11-23 17:32:10 +0100

    add Iteration#1

---
+++ script.js
@@ -0,0 +1,13 @@
+// 1.1 Crea una variable llamada myFavoriteHero, asigna el valor Hulk a ella.
+// 1.2 Crea una variable llamada x, asigna el valor 50 a ella.
+// 1.3 Crea una variable llamada 'h' con el valor 5 y otra 'y' con el valor 10. 
+// 1.4 Crea una otra variable 'z' y asignale el valor de 'h' + 'y'.
+
+var myFavoriteHero = "Hulk";
+    x = 50;
+    h = 5;
+    y = 10;
+    z = h + y ;
+    total = z;
+    console.log(total);
+    
