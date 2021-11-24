4da394c79d1c3b3872d378b71efdb840e0a9054a
Carmen Blanco <menxuuu02@gmail.com>
2021-11-24 09:39:38 +0100

    add Iteración #3

---
+++ script3.js
@@ -0,0 +1,41 @@
+// 1.1 Multiplica 10 por 5 y muestra el resultado mediante alert.
+let num1 = 10;
+    num2 = 5;
+    operacioMulti = num1 * num2 ;
+    resultadoMulti = operacioMulti;
+
+    alert(resultadoMulti);
+
+    // 1.2 Divide 10 por 2 y muestra el resultado en un alert.
+
+    num3 = 2;
+    operacionDiv =num1 / num3 ;
+    resultadoDiv = operacionDiv;
+    
+    alert(resultadoDiv);
+
+    // 1.3 Muestra mediante un alert el resto de dividir 15 por 9.
+    
+    resto = 15 / 9 ;
+    alert(resto);
+
+//  1.4 Usa el correcto operador de asignación que resultará en x = 15, 
+// teniendo dos variables y = 10 y z = 5.
+
+let y = 10;
+    z = 5;
+    x = y += z ;
+    console.log(x);
+
+//  1.5 Usa el correcto operador de asignación que resultará en x = 50,
+// teniendo dos variables y = 10 y z = 5.
+ x = y *= z ;
+ console.log(x);
+
+ 
+
+
+
+
+
+
